97c37e35948204da049c6413d3dd2d37ca395267
Tdejong765 <tdejong765@gmail.com>
2023-11-23 22:56:23 +0100

    First working gameoflife version

--- gameoflife.py
+++ gameoflife.py
@@ -1,12 +1,11 @@
 
+import random
 import sys
 import pygame
 
 clock = pygame.time.Clock()
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
-boardArr = []
-newArr = []
 windowHeight = 400
 windowWidth = 400
 blockSize = 20
@@ -20,13 +19,22 @@ def drawGrid():
             pygame.draw.rect(SCREEN, BLACK, rect, 1)
 
 
+#Print grid based on value of the different cells
+def print_grid(rows, cols, grid):
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 0:
+                drawBlockBlack(rows, cols)
+            if grid[row][col] == 1:
+                drawBlockWhite(rows, cols)
+   
 
 #Draws white block in pygame graphics
 def drawBlockWhite(row, col):
             cords = convert(row, col)
             rect = pygame.Rect(cords[0], cords[1], blockSize, blockSize)
             pygame.draw.rect(SCREEN, WHITE, rect, 10)
-            boardArr[row][col] = 1
+
 
 
 #Draws black block in pygame graphics
@@ -34,7 +42,6 @@ def drawBlockBlack(row, col):
             cords = convert(row, col)
             rect = pygame.Rect(cords[0], cords[1], blockSize, blockSize)
             pygame.draw.rect(SCREEN, BLACK, rect, 10)
-
 
 
 #Converts normal index places to pygame coordinate places
@@ -46,93 +53,43 @@ def convert(row, col):
 
 #Creates boardarray to keep track of cell status
 def initBoardArr():
-    colW = windowWidth // 20
-    rowH = windowHeight // 20
-
-    for i in range(colW):
-        boardArr.append([])
-        newArr.append([])
-        for j in range(rowH):
-            boardArr[i].append(0)
-            newArr[i].append(0)
+    boardArray = []
+    for row in range(20):
+            rows = []
+            for col in range(20):
+                if random.randint(0, 7) == 0:
+                    rows += [1]
+                else:
+                    rows += [0]
+            boardArray += [rows]
+    return boardArray
 
 
 #Count number of neigbours of a cell
-def countNeighbours(array, i, j):
-
+def countNeighbours(array, row, col):
     count = 0
-
-    if (array[i][j] == 1):
-        count +=1
-
-    if (array[i-1][j-1] == 1):
-        count +=1
-
-    if (array[i-1][j] == 1):
-        count +=1
-
-    if (array[i-1][j+1] == 1):
-        count +=1
-
-    if (array[i][j-1] == 1):
-        count +=1
-
-    if (array[i][j+1] == 1):
-        count +=1
-
-    if (array[i+1][j-1] == 1):
-        count +=1
-
-    if (array[i+1][j] == 1):
-        count +=1
-
-    if (array[i+1][j+1] == 1):
-        count +=1
-
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if not (i == 0 and j == 0):
+                count += array[((row + i) % 20)][((col + j) % 20)]
     print(count)
-
     return count
 
 
+#Updates the current grid based on the value of the different cells, calls the countNeighbour function and applies the gameoflife rules
+def Update(boardArr, numRows, numCols):
+    for row in range(numRows):
+        for col in range(numCols):
 
-def Update(boardArr, newArr, i, j):
+            count = countNeighbours(boardArr, row, col)
 
-    # #apply rules
-    # if (array[i][j] == 1 and count <2):
-    #     array[i][j] == 0
-    #     drawBlockBlack(i, j)
+            if count < 2 or count > 3:
+                boardArr[row][col] = 0
+                drawBlockBlack(row,col)
 
-    # if (array[i][j] == 1 and count == 2 or count == 3):
-    #     array[i][j] == 1
-    #     drawBlockWhite(i, j)
-
-    # if (array[i][j] == 1 and count > 3):
-    #     array[i][j] == 0
-    #     drawBlockBlack(i, j)
-
-    # if (array[i][j] == 0 and count == 3):
-    #     array[i][j] == 1
-    #     drawBlockWhite(i, j)
-
-    for row in range(i):
-        for col in range(j):
-            # Get the number of live cells adjacent to the cell at grid[row][col]
-            live_neighbors = countNeighbours(boardArr, i, j)
-
-            # If the number of surrounding live cells is < 2 or > 3 then we make the cell at grid[row][col] a dead cell
-            if live_neighbors < 2 or live_neighbors > 3:
-                newArr[row][col] = 0
-                drawBlockBlack(row, col)
-
-            # If the number of surrounding live cells is 3 and the cell at grid[row][col] was previously dead then make
-            # the cell into a live cell
-            elif live_neighbors == 3 and boardArr[row][col] == 0:
-                newArr[row][col] = 1
-                drawBlockWhite(row, col)
-            # If the number of surrounding live cells is 3 and the cell at grid[row][col] is alive keep it alive
-            else:
-                newArr[row][col] = boardArr[row][col]
-
+            elif count == 3 and boardArr[row][col] == 0:
+                boardArr[row][col] = 1
+                drawBlockWhite(row,col)
 
 
 #Main loop for refreshing board status        
@@ -142,17 +99,14 @@ def main():
     SCREEN = pygame.display.set_mode((windowWidth, windowHeight))
     CLOCK = pygame.time.Clock()
     SCREEN.fill(BLACK)
-    initBoardArr()
-    drawBlockWhite(2,2)
-    drawBlockWhite(2,3)
-    drawBlockWhite(2,4)
-    drawBlockWhite(1,3)
-    drawBlockWhite(1,4)
-    drawBlockWhite(3,2)
 
+    # Create the initial random Game of Life grids
+    grid = initBoardArr()
+ 
     while True:
-        drawGrid()
-        Update(boardArr, newArr, 2, 3)
+        print_grid(20,20, grid)
+        Update(grid, 20, 20)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
